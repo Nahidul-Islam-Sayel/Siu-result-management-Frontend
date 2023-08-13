4df90772b22cbf46559f877da5ef86063d9e9a59
Your Name <you@example.com>
2023-08-13 15:38:05 +0600

    update 13/08/2023

--- src/Component/TeachersProfile/TeachersProfile.jsx
+++ src/Component/TeachersProfile/TeachersProfile.jsx
@@ -1,6 +1,7 @@
 import { Line } from 'rc-progress';
 import React, { useEffect, useState } from "react";
 import PieChart from 'react-pie-graph-chart';
+import Profile from '../../asset/Image/SIULOGO.png';
 const TeachersProfile = () => {
     const [teachersBackground, setTeachersBackground] = useState([]);
     useEffect(() => {
@@ -19,40 +20,43 @@ const TeachersProfile = () => {
     }, []);
     return (
         <div className="container mt-5 mb-10">
+
             <div className="row">
-                <div class="col-md-4 mb-3 xyz">
+
+                {teachersBackground[0] && <div class="col-md-4 mb-3 xyz">
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex flex-column align-items-center text-center">
-                                <img src="" alt="Admin" class="rounded-circle" width="150" />
+                                <img src={Profile} alt="Admin" class="rounded-circle" width="100" />
                                 <div class="mt-3">
-                                    <h4 className='text-3xl font-serif'>Department Of</h4>
-                                    <h4 className='text-4xl font-mono'></h4>
-                                    <p class="text-secondary mb-1">Admin</p>
-
+                                    <h4 className='text-2xl font-serif'>{teachersBackground[0].name && teachersBackground[0].name}</h4>
+                                    <h4 className='text-xl font-serif'>{teachersBackground[0].position && teachersBackground[0].position}</h4>
+                                    <h4 className='text-xl font-serif'>Department Of  {teachersBackground[0].department && teachersBackground[0].department}</h4>
+                                    <p class="text-3xl font-serif"></p>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     <div class="card mt-3">
                         <ul class="list-group list-group-flush">
 
                             <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0">Email</h6>
-                                <span class="text-secondary"></span>
+                                <span class="text-secondary">{teachersBackground[0].email && teachersBackground[0].email}</span>
                             </li>
 
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0">Phone</h6>
+                            {/* <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0"></h6>
                                 <span class="text-secondary"></span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            </li> */}
+                            {/* <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        {/* <NavLink to="/EdiProfile"><a class="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit Profile</a></NavLink>    */}
+                                        <NavLink to="/EdiProfile"><a class="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit Profile</a></NavLink>
                                     </div>
                                 </div>
-                            </li>
+                            </li> */}
                             {/* <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                     <div class="row">
                                         <div class="col-sm-12">
@@ -66,14 +70,14 @@ const TeachersProfile = () => {
                         </ul>
                     </div>
 
-                </div>
+                </div>}
                 <div className="col-md-8">
                     <div className="row">
                         <div className="col-md-4">
                             <div class="card text-white bg-info" >
-                                <div class="card-header">Total FACULTY MEMBERS</div>
+                                <div class="card-header">Total Assign Course</div>
                                 <div class="card-body">
-                                    <h5 class="card-title"></h5>
+                                    <h5 class="card-title">{ }</h5>
                                     <p class="card-text"><img src="https://cdn.pixabay.com/photo/2016/03/31/20/40/arrow-1295953_960_720.png" alt="" /></p>
                                 </div>
 
@@ -93,29 +97,23 @@ const TeachersProfile = () => {
                     </div>
                     <div className='row'>
                         <div className="col-md-7">
-                            <h5>Doctor</h5>
+                            <h5>Regular</h5>
                             <Line percent="" strokeWidth={2} strokeColor="#E97D30" />
-                            <h5>User</h5>
+                            <h5>Retake</h5>
                             <Line percent="" strokeWidth={2} strokeColor="#62B170" />
-                            <h5>Blood Donner And Volunteer</h5>
-                            <Line percent="" strokeWidth={2} strokeColor="#F1AF20" />
+
                         </div>
                         <div className="col-md-5">
                             <PieChart data={[
                                 {
-                                    type: "Doctor",
+                                    type: "Regular",
                                     value: 10,
                                     color: "#E97D30"
                                 },
                                 {
-                                    type: "User",
+                                    type: "Retake",
                                     value: 10,
                                     color: "#62B170"
-                                },
-                                {
-                                    type: "Report",
-                                    value: 10,
-                                    color: "#F1AF20"
                                 }
                             ]} />
                         </div>
@@ -126,30 +124,38 @@ const TeachersProfile = () => {
                 </div>
             </div>
             <div className="container">
-                <h5 class="text-4xl text-blod font-serif font-bold text-center mt-5">FACULTY MEMBERS</h5>
-                <div className="row">
-
-                    <div className="col-md-4 mt-5 ">
-                        <div class="card" >
-                            <div class="card-body">
-                                <h5 class="text-2xl text-blod font-serif font-bold"></h5>
-                                <h6 class="text-xl italic font-bold"></h6>
-                                <h6 class="text-lg italic font-bold">Department Of </h6>
-
-                                <button type="button" class="btn btn-outline-info m=2" >View Profile</button>
-
-
-                                <button type="button" class="btn btn-outline-info m-2" >Update</button>
-
-
-
-                                <button type="button" class="btn btn-outline-info m-2">Delate</button>
-
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <h5 class="text-4xl text-blod font-serif font-bold text-center mt-5">Assigned Course</h5>
+                <br /><br />
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
